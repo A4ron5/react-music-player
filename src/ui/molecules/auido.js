c86587a34127ee5495df7281fb511e188d3ef255
Max Safin <a4ron5@yandex.ru>
2019-12-07 19:50:14 +0500

    Delete unnecessary files

--- src/ui/molecules/auido.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-export const Audio = ({ src }) => (
-  <audio id='audio'>
-    <source src='http://d.zaix.ru/g72i.mp3'/>
-    Your browser does not support the <code>audio</code> element.
-  </audio>
-)
